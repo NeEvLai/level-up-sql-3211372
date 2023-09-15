67aac2f7c1a3aa7c36a1558ca7e7ab3af77a2f1e
NeEvLai <126679781+NeEvLai@users.noreply.github.com>
2023-09-15 11:16:42 +0000

    my solution to 0201

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,17 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+select * from Books
+where Title like '%Dracula%';
+
+SELECT
+  (select count(Title) -- total number of copies
+    from Books
+    where Title = 'Dracula') -
+  (select count(B.Title) -- not returned copies
+    from Loans L
+    inner join Books B
+    on L.BookID = B.BookID
+    where B.Title = 'Dracula'
+    and L.ReturnedDate is null) 
+as AvailableCopies;
