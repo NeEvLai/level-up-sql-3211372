81c33aff4536a40411f7bc879242e73315899053
NeEvLai <126679781+NeEvLai@users.noreply.github.com>
2023-09-15 10:25:06 +0000

    solution to challenge 10

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,26 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+select * from Customers
+where FirstName = 'Loretta';
+
+insert into Orders (CustomerID, OrderDate)
+values (70, '2022-09-20 14:00:00');
+
+select *
+from Orders
+where CustomerID = 70
+order by OrderDate desc;
+
+insert into OrdersDishes (OrderID, DishID)
+values 
+(1001,(select DishID from Dishes where Name = 'House Salad')),
+(1001,(select DishID from Dishes where Name = 'Mini Cheeseburgers')),
+(1001,(select DishID from Dishes where Name = 'Tropical Blue Smoothie'));
+
+select sum(D.Price)
+from Dishes D
+inner join OrdersDishes OD
+on D.DishID = OD.DishID
+where OD.OrderID = 1001;
