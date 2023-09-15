117fde94f436b14563d06da2fc6b082b72f59b54
NeEvLai <126679781+NeEvLai@users.noreply.github.com>
2023-09-15 10:32:53 +0000

    solution for challenge 11

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,11 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+update Customers
+set FavoriteDish = (
+  select DishID
+  from Dishes
+  where Name = 'Quinoa Salmon Salad')
+where FirstName = 'Cleo'
+and Lastname = 'Goldwater';
